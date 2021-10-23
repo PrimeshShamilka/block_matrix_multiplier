6244108a68e88005e197d129a353299ea377a009
Kirishikesan Kannan <kirishikesan.17@cse.mrt.ac.lk>
2021-10-23 17:34:33 +0530

    Fixed casting error in matrix multiplier

--- block_matrix_multiplier.srcs/sources_1/new/martix_multiplier_2x2.vhd
+++ block_matrix_multiplier.srcs/sources_1/new/martix_multiplier_2x2.vhd
@@ -17,48 +17,43 @@ architecture Behavioral of mat_mul_2x2 is
 type row_2x2 is array (0 to 1) of unsigned (7 downto 0);
 type mat_2x2 is array (0 to 1) of row_2x2;
 signal mat_A,mat_B,mat_C: mat_2x2 := (others => (others => X"00"));
+type state_type is (init,do_mult);
+signal state : state_type := init;
+
 begin
 
 process(clk,reset)
+variable temp : unsigned(7 downto 0) := (others => '0');
 begin
     if (reset = '1') then
         mat_A <= (others => (others => X"00")); 
         mat_B <= (others => (others => X"00"));
     elsif(rising_edge(clk)) then
-        for i in 0 to 1 loop
-            for j in 0 to 1 loop
-                mat_A(i)(j)<= A(((i*2+j+1)*8-1) downto ((i*2+j)*8));
-                mat_B(i)(j)<= B(((i*2+j+1)*8-1) downto ((i*2+j)*8));
-            end loop;
-        end loop;
-        for i in 0 to 1 loop
-            for j in 0 to 1 loop
-                mat_C(i)(j)<= mat_A(i)(0)*mat_B(0)(j) + mat_A(i)(1)*mat_B(1)(j);
-            end loop;
-        end loop;
-        for i in 0 to 1 loop
-            for j in 0 to 1 loop
-                C((i*2+j+1)*8-1 downto (i*2+j)*8) <= mat_C(i)(j);
-                C((i*2+j+1)*8-1 downto (i*2+j)*8) <= mat_C(i)(j);
-            end loop;
---            if(i=31) then
---                done<='1';
---            end if;
-        end loop;
---        for i in 0 to 31 loop
---            if(i<8) then
---                C(i downto (i-1)) <= mat_C(0)(0)(7-i);
---            elsif(i<16) then
---                C(i downto (i-1)) <= mat_C(0)(15- (i mod 8));
---            elsif(i<24) then
---                C(i downto (i-1)) <= mat_C(1)(7- (i mod 8));
---            else
---                C(i downto (i-1)) <= mat_C(1)(15- (i mod 8));
---            end if;
---            if(i=31) then
---                done<='1';
---            end if;
---        end loop;
+        case state is
+            when init=>
+                for i in 0 to 1 loop
+                    for j in 0 to 1 loop
+                        mat_A(i)(j)<= A(((i*2+j+1)*8-1) downto ((i*2+j)*8));
+                        mat_B(i)(j)<= B(((i*2+j+1)*8-1) downto ((i*2+j)*8));
+                    end loop;
+                end loop;
+                state<=do_mult;
+            when do_mult=>
+                
+                for i in 0 to 1 loop
+                    for j in 0 to 1 loop
+                        mat_C(i)(j)<= to_unsigned(to_integer(mat_A(i)(0))*to_integer(mat_B(0)(j)),8)+to_unsigned(to_integer(mat_A(i)(1))*to_integer(mat_B(1)(j)),8);
+--                         + mat_A(i)(1)*to_integer(mat_B(1)(j));
+--                        mat_C(i)(j)<= to_unsigned(mat_B(i)(j)(7 downto 0),8)*to_unsigned(mat_A(i)(j)(7 downto 0),8);
+                    end loop;
+                end loop;
+                for i in 0 to 1 loop
+                    for j in 0 to 1 loop
+                        C((i*2+j+1)*8-1 downto (i*2+j)*8) <= mat_C(i)(j);
+                    end loop;
+                end loop;
+                done<='1';
+          end case;
     end if;
 end process;
 
